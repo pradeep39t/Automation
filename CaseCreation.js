bbc01fb8b3bdff4b4cdc22a672d26cfdbddce7c4
pradeep39t <pradeep.panigrahy_nist@yahoo.com>
2017-07-06 00:51:48 +0530

    Update CaseCreation.js

--- CaseCreation.js
+++ CaseCreation.js
@@ -1,15 +1,3 @@
  ;(function(){
-    if(document.getElementById('PRINTTITLE').innerHTML=='Register Application'){
-      appReg();
-      }
- 
-    var appReg = function(){
-      document.getElementsByName('monthdateReceived')[0].value=today.getMonth()+1;
-      document.getElementsByName('datedateReceived')[0].value=today.getDate();
-      document.getElementsByName('yeardateReceived')[0].value=today.getFullYear();
-      document.getElementsByName('appModeCd')[0].value='WI';
-      document.getElementsByName('firstName')[0].value='Suzanne';
-      document.getElementsByName('lastName')[0].value='Shaw';
-      document.getElementById('button2');
-      }
+    alert('HelloWorld');
     })(window)
