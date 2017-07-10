78c9243c7974c304bc049322bcb61a56648a95ad
pradeep39t <pradeep.panigrahy_nist@yahoo.com>
2017-07-11 01:30:19 +0530

    Update CaseCreation.js

--- CaseCreation.js
+++ CaseCreation.js
@@ -9,7 +9,7 @@ function myFunction(){
       document.getElementsByName('datedateReceived')[0].value=today.getDate();
       document.getElementsByName('yeardateReceived')[0].value=today.getFullYear();
       document.getElementsByName('appModeCd')[0].value='WI';
-	  document.body.appendChild(document.createElement('script')).src='http://marak.com/faker.js/js/faker.js';
+      document.body.appendChild(document.createElement('script')).src='http://marak.com/faker.js/js/faker.js';
       document.getElementsByName('firstName')[0].value=faker.name.firstName();
       document.getElementsByName('lastName')[0].value=faker.name.lastName();
       document.getElementById('button2');
