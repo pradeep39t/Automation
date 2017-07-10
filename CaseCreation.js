044a7f9d2a5e9db345f0593a7bf9590fe1de08a3
pradeep39t <pradeep.panigrahy_nist@yahoo.com>
2017-07-11 01:29:35 +0530

    Update CaseCreation.js

--- CaseCreation.js
+++ CaseCreation.js
@@ -1,15 +1,17 @@
 function myFunction(){
+	var today=new Date();
+    if(document.getElementById('PRINTTITLE').innerHTML=='Register Application'){
+      appReg();
+      }
+ 
     var appReg = function(){
       document.getElementsByName('monthdateReceived')[0].value=today.getMonth()+1;
       document.getElementsByName('datedateReceived')[0].value=today.getDate();
       document.getElementsByName('yeardateReceived')[0].value=today.getFullYear();
       document.getElementsByName('appModeCd')[0].value='WI';
-      document.getElementsByName('firstName')[0].value='Suzanne';
-      document.getElementsByName('lastName')[0].value='Shaw';
+	  document.body.appendChild(document.createElement('script')).src='http://marak.com/faker.js/js/faker.js';
+      document.getElementsByName('firstName')[0].value=faker.name.firstName();
+      document.getElementsByName('lastName')[0].value=faker.name.lastName();
       document.getElementById('button2');
-      };
-    if(document.getElementById('PRINTTITLE').innerHTML=='Register Application'){
-      appReg();
       }
- 
     }myFunction()
