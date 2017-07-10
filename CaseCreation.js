41c12fcb4c4dcd46814bcb505e661f050c892bbf
pradeep39t <pradeep.panigrahy_nist@yahoo.com>
2017-07-11 01:53:05 +0530

    Update CaseCreation.js

--- CaseCreation.js
+++ CaseCreation.js
@@ -1,16 +1,24 @@
 function myFunction(){
-    var today=new Date();
-    var appReg = function(){
-    document.getElementsByName('monthdateReceived')[0].value=today.getMonth()+1;
-    document.getElementsByName('datedateReceived')[0].value=today.getDate();
-    document.getElementsByName('yeardateReceived')[0].value=today.getFullYear();
-    document.getElementsByName('appModeCd')[0].value='WI';
-    document.body.appendChild(document.createElement('script')).src='http://marak.com/faker.js/js/faker.js';
-    document.getElementsByName('firstName')[0].value=faker.name.firstName();
-    document.getElementsByName('lastName')[0].value=faker.name.lastName();
-    document.getElementById('button2');
-    }	
+	var today=new Date();
+	
+	function loadScript(url){    
+		var head = document.getElementsByTagName('head')[0];
+		var script = document.createElement('script');
+		script.type = 'text/javascript';
+		script.src = url;
+		head.appendChild(script);
+	}
+	var appReg = function(){
+      document.getElementsByName('monthdateReceived')[0].value=today.getMonth()+1;
+      document.getElementsByName('datedateReceived')[0].value=today.getDate();
+      document.getElementsByName('yeardateReceived')[0].value=today.getFullYear();
+      document.getElementsByName('appModeCd')[0].value='WI';
+	  loadScript('http://marak.com/faker.js/js/faker.js');
+      document.getElementsByName('firstName')[0].value=faker.name.firstName();
+      document.getElementsByName('lastName')[0].value=faker.name.lastName();
+      document.getElementById('button2');
+    }
     if(document.getElementById('PRINTTITLE').innerHTML=='Register Application'){
       appReg();
-    }
+      }    
 }myFunction()
