66306aef94b2dd4c9513fbd3c140fa70b33bc6da
pradeep39t <pradeep.panigrahy_nist@yahoo.com>
2017-07-11 08:32:29 +0530

    Update CaseCreation.js

--- CaseCreation.js
+++ CaseCreation.js
@@ -39,6 +39,8 @@ function myFunction(){
 		document.getElementById('button4').click();
 	}	
 	if(document.getElementById('PRINTTITLE').innerHTML=='Register Program - Program'){
+		document.getElementById('cash').checked;
+		document.getElementById('SNAP').checked;
 		document.getElementById('maFormCd').value='HCS-250';
 	}
 	if(document.getElementById('PRINTTITLE').innerHTML=='Register Application - Summary'){
