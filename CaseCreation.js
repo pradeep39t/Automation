e13ae111b7921040406677983bf35b9d128d53e7
pradeep39t <pradeep.panigrahy_nist@yahoo.com>
2017-08-08 09:22:05 +0530

    Update CaseCreation.js

--- CaseCreation.js
+++ CaseCreation.js
@@ -1,4 +1,4 @@
-function myFunction(){
+(function myFunction(){
 	var today=new Date();
 	if(document.getElementById('PRINTTITLE').innerHTML=='Register Application'){
 		document.getElementsByName('monthdateReceived')[0].value=today.getMonth()+1;
@@ -257,5 +257,6 @@ function myFunction(){
 		document.getElementById('issuanceMethod_0').value='EB';
 		document.getElementById('additionalProtectivePayee_0').value='N';
 		document.getElementById('actionButtonNext').click();
-	}	
-}myFunction()
+	}
+	setTimeout(myFunction, 10000);
+})()
