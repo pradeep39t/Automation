d4298b7b0024d6adcde7dde12224c8c531e96946
pradeep39t <pradeep.panigrahy_nist@yahoo.com>
2017-08-23 12:59:10 +0530

    Update CaseCreation.js

--- CaseCreation.js
+++ CaseCreation.js
@@ -119,7 +119,7 @@ function myFunction(){
 		document.getElementById('actionButtonNext').click()
 	}
 	if(document.getElementById('PRINTTITLE').innerHTML=='Household Individuals - Questions'){		
-		document.getElementsByName('currentlyPregnant')[0].value='Y';
+		document.getElementsByName('currentlyPregnant')[0].value='N';
 		document.getElementsByName('medDACPickleQuestion')[0].value='N';
 		document.getElementsByName('medQualWorkActivityQuestion')[0].value='N';
 		document.getElementsByName('acaAdultCoverageQuestion')[0].value='N';
@@ -168,7 +168,7 @@ function myFunction(){
 		document.getElementById('lifeInsuranceResSw').value='N';
 		document.getElementById('rtcheck4').value='N';
 		document.getElementById('unusualValResSw').value='N';
-		document.getElementById('livestockResSw').value='Y';
+		document.getElementById('livestockResSw').value='N';
 		document.getElementById('mineralRightsResSw').value='N';
 		document.getElementById('rtcheck2').value='N';
 		document.getElementsByName('assetTransferSw')[0].value='N';
@@ -210,7 +210,7 @@ function myFunction(){
 	}	
 	if(document.getElementById('PRINTTITLE').innerHTML=='Income Questions'){
 		document.getElementById('incomeEDTestSw').value='N';
-		document.getElementById('rtcheck3').value='Y';
+		document.getElementById('rtcheck3').value='N';
 		document.getElementById('rtcheck1').value='N';
 		document.getElementsByName('isSelfEmployed')[0].value='N';
 		document.getElementsByName('additionalIncomeExpenses')[0].value='N';
